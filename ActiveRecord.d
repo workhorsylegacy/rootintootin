0b3a2351af5f050c3c6bfe2d045ee58fc1264d77
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-10-04 16:03:58 -0700

    Added basic controller. Added server example. Can only accept connections. Uber ugly code.

--- ActiveRecord.d
+++ ActiveRecord.d
@@ -12,6 +12,7 @@
 */
 
 import std.stdio;
+import std.socket;
 //import mysql;
 //import mysql_wrapper;
 
@@ -87,6 +88,7 @@ public class Field(T) {
 	public void opAssign(T value) {
 		_value = value;
 	}
+
 	public T opCall() {
 		return _value;
 	}
@@ -99,13 +101,22 @@ template ModelBaseMixin(T, string table_name) {
 		string query = "select * from " ~ typeof(T)._table_name ~ " where id=" ~ std.string.toString(id);
 		return 7;
 	}
+
+	static T[] find_all() {
+		T[] ts = [];
+		return ts;
+	}
+
+	static T find_first() {
+		return null;
+	}
 }
 
 public class User : ModelBase {
 	mixin ModelBaseMixin!(User, "user");
 
-	private Field!(string) name = null;
-	private Field!(bool) hide_email_address = null;
+	public Field!(string) name = null;
+	public Field!(bool) hide_email_address = null;
 
 	public this() {
 		name = new Field!(string)("name");
@@ -113,11 +124,161 @@ public class User : ModelBase {
 	}
 }
 
+template ControllerBase(T) {
+	Object[][string] _members;
+	Object[string] _member;
+
+	void set(T)(string key, T thing) {
+		_member[key] = cast(Object) thing;
+	}
+
+	void set_array(T)(string key, T thing) {
+		_members[key] = cast(Object[]) thing;
+	}
+
+	T get(T)(string key) {
+		return cast(T) _member[key];
+	}
+
+	T get_array(T)(string key) {
+		return cast(T) _members[key];
+	}
+}
+
+public class UserController {
+	mixin ControllerBase!(UserController);
+
+	public void index() {
+		User[] users = User.find_all();
+		User user = new User();
+		user.name = "bobrick";
+		users ~= user;
+
+		set_array!(User[])("users", users);
+		set!(User)("user", user);
+
+
+		users = get_array!(User[])("users");
+		user = get!(User)("user");
+	}
+}
+
+
 void main() {
+	/*
+     respStatus.Add(200, "200 Ok");
+     respStatus.Add(201, "201 Created");
+     respStatus.Add(202, "202 Accepted");
+     respStatus.Add(204, "204 No Content");
+
+     respStatus.Add(301, "301 Moved Permanently");
+     respStatus.Add(302, "302 Redirection");
+     respStatus.Add(304, "304 Not Modified");
+     
+     respStatus.Add(400, "400 Bad Request");
+     respStatus.Add(401, "401 Unauthorized");
+     respStatus.Add(403, "403 Forbidden");
+     respStatus.Add(404, "404 Not Found");
+
+     respStatus.Add(500, "500 Internal Server Error");
+     respStatus.Add(501, "501 Not Implemented");
+     respStatus.Add(502, "502 Bad Gateway");
+     respStatus.Add(503, "503 Service Unavailable");
+
+	*/
+	TcpSocket listener = new TcpSocket(); //(AddressFamily.INET);
+	listener.blocking = false;
+	listener.bind(new InternetAddress(5678));
+	listener.listen(10);
+
+	const int MAX_CONNECTIONS = 60;
+	SocketSet sset = new SocketSet(MAX_CONNECTIONS + 1); // Room for listener.
+	Socket[] reads;
+	
+	for(;; sset.reset()) {
+		sset.add(listener);
+		foreach(Socket each; reads) {
+			sset.add(each);
+		}
+		
+		Socket.select(sset, null, null);
+
+		int i;
+		for(i = 0;; i++) {
+			next:
+			if(i == reads.length)
+				break;
+
+			if(sset.isSet(reads[i])) {
+				char[1024] buf;
+				int read = reads[i].receive(buf);
+				if(Socket.ERROR == read) {
+					printf("Connection error.\n");
+					goto sock_down;
+				} else if(0 == read) {
+					try {
+						//if the connection closed due to an error, remoteAddress() could fail
+						printf("Connection from %.*s closed.\n", reads[i].remoteAddress().toString());
+					} catch {
+					}
+
+					sock_down:
+					reads[i].close(); //release socket resources now
+
+					//remove from -reads-
+					if(i != reads.length - 1)
+						reads[i] = reads[reads.length - 1];
+					reads = reads[0 .. reads.length - 1];
+					
+					printf("\tTotal connections: %d\n", reads.length);
+					
+					goto next; //-i- is still the next index
+				} else {
+					printf("Received %d bytes from %.*s: \"%.*s\"\n", read, reads[i].remoteAddress().toString(), buf[0 .. read]);
+				}
+			}
+		}
+
+
+		//connection request
+		if(sset.isSet(listener)) {
+			Socket sn;
+			try {
+				if(reads.length < MAX_CONNECTIONS) {
+					sn = listener.accept();
+					printf("Connection from %.*s established.\n", sn.remoteAddress().toString());
+					assert(sn.isAlive);
+					assert(listener.isAlive);
+					
+					reads ~= sn;
+					printf("\tTotal connections: %d\n", reads.length);
+				} else {
+					sn = listener.accept();
+					printf("Rejected connection from %.*s; too many connections.\n", sn.remoteAddress().toString());
+					assert(sn.isAlive);
+					
+					sn.close();
+					assert(!sn.isAlive);
+					assert(listener.isAlive);
+				}
+			} catch(Exception e) {
+				printf("Error accepting: %.*s\n", e.toString());
+				
+				if(sn)
+					sn.close();
+			}
+		}
+	}
+
+	UserController controller = new UserController();
+	controller.index();
+
+	int[] a = [1, 2, 3, 4, 5];
 	// NOTE: to convert a string to a char* use std.string.toStringz(s1)
 	//ModelBase.connect_to_database("thing.db");
 	
 	User user = new User();
+	writefln("%d", user.sizeof); 
 	user.name = "first name";
 	writefln("[%s]", user.find_by_id(7));
 	writefln("[%s]", user.name());
