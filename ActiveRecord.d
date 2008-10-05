4835e68f67da658e188a1a7796fee30953f47a2c
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-10-04 17:34:35 -0700

    Changed the webserver so it can reply and close connections.

--- ActiveRecord.d
+++ ActiveRecord.d
@@ -186,57 +186,50 @@ void main() {
      respStatus.Add(503, "503 Service Unavailable");
 
 	*/
-	TcpSocket listener = new TcpSocket(); //(AddressFamily.INET);
+	TcpSocket listener = new TcpSocket();
 	listener.blocking = false;
-	listener.bind(new InternetAddress(5678));
+	listener.bind(new InternetAddress(2345));
 	listener.listen(10);
 
 	const int MAX_CONNECTIONS = 60;
 	SocketSet sset = new SocketSet(MAX_CONNECTIONS + 1); // Room for listener.
 	Socket[] reads;
-	
-	for(;; sset.reset()) {
+
+	while(true) {
+		sset.reset();
 		sset.add(listener);
 		foreach(Socket each; reads) {
 			sset.add(each);
 		}
-		
 		Socket.select(sset, null, null);
 
-		int i;
-		for(i = 0;; i++) {
-			next:
-			if(i == reads.length)
-				break;
-
-			if(sset.isSet(reads[i])) {
-				char[1024] buf;
-				int read = reads[i].receive(buf);
-				if(Socket.ERROR == read) {
-					printf("Connection error.\n");
-					goto sock_down;
-				} else if(0 == read) {
-					try {
-						//if the connection closed due to an error, remoteAddress() could fail
-						printf("Connection from %.*s closed.\n", reads[i].remoteAddress().toString());
-					} catch {
-					}
-
-					sock_down:
-					reads[i].close(); //release socket resources now
-
-					//remove from -reads-
-					if(i != reads.length - 1)
-						reads[i] = reads[reads.length - 1];
-					reads = reads[0 .. reads.length - 1];
-					
-					printf("\tTotal connections: %d\n", reads.length);
-					
-					goto next; //-i- is still the next index
-				} else {
-					printf("Received %d bytes from %.*s: \"%.*s\"\n", read, reads[i].remoteAddress().toString(), buf[0 .. read]);
-				}
+		for(int i=0; i<reads.length; i++) {
+			if(sset.isSet(reads[i]) == false) {
+				continue;
 			}
+
+			char[1024] buf;
+			int read = reads[i].receive(buf);
+
+			if(Socket.ERROR == read) {
+				printf("Connection error.\n");
+			} else if(0 == read) {
+				try {
+					//if the connection closed due to an error, remoteAddress() could fail
+					printf("Connection from %.*s closed.\n", reads[i].remoteAddress().toString());
+				} catch {
+				}
+			} else {
+				printf("Received %d bytes from %.*s: \"%.*s\"\n", read, reads[i].remoteAddress().toString(), buf[0 .. read]);
+				reads[i].send("<html><body><h1>lookie! HTMLs!</h1></body></html>");
+			}
+
+			//remove from reads
+			reads[i].close();
+			if(i != reads.length - 1)
+				reads[i] = reads[reads.length - 1];
+			reads = reads[0 .. reads.length - 1];
+			printf("\tTotal connections: %d\n", reads.length);
 		}
 
 
@@ -249,21 +242,21 @@ void main() {
 					printf("Connection from %.*s established.\n", sn.remoteAddress().toString());
 					assert(sn.isAlive);
 					assert(listener.isAlive);
-					
+				
 					reads ~= sn;
 					printf("\tTotal connections: %d\n", reads.length);
 				} else {
 					sn = listener.accept();
 					printf("Rejected connection from %.*s; too many connections.\n", sn.remoteAddress().toString());
 					assert(sn.isAlive);
-					
+				
 					sn.close();
 					assert(!sn.isAlive);
 					assert(listener.isAlive);
 				}
 			} catch(Exception e) {
 				printf("Error accepting: %.*s\n", e.toString());
-				
+			
 				if(sn)
 					sn.close();
 			}
