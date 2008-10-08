518fa4846c6e20869a718ab1df0767ca606f228e
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-10-08 07:12:50 -0700

    Fixed the embedded D compiler to generate working D templates.

--- ed_compiler.py
+++ ed_compiler.py
@@ -4,9 +4,12 @@ f = open('index.html.ed', 'r')
 output = []
 
 body = f.read()
+f.close()
 
-open_bracket = -1
-open_write = -1
+output.append(
+"public string render(UserController controller, out int[int] line_translations) { \n" +
+"	// Generate the view as an array of strings\n" +
+"	string[] builder;")
 
 while len(body) > 0:
 	open_index = body.find("<%")
@@ -15,24 +18,39 @@ while len(body) > 0:
 	# raise if open but no close
 	# raise if close before open
 
-	if open_index == -1:
-		break
+	#if open_index == -1:
+	#	break
 
 	before = body[: open_index]
 	middle = body[open_index+2 : close_index]
 	after = body[close_index+2 :]
 
+	if open_index == -1 and close_index == -1:
+		output.append("\n	builder ~= \"" + body.replace("\"", "\\\"") + "\"; ")
+		break
+
 	if len(before) > 0:
-		output += "builder ~= \"" + before + "\"; "
-		print "builder ~= \"" + before + "\"; "
+		output.append("\n	builder ~= \"" + before.replace("\"", "\\\"") + "\"; ")
 
 	if len(middle) > 0:
 		if middle[0] == "=":
-			output += middle
-			print middle
+			output.append("\n	builder ~= " + middle[1:].replace("\"", "\\\"") + "; ")
 		else:
-			output +=  "builder ~= \"" + middle + "\"; "
-			print  "builder ~= \"" + middle + "\"; "
+			output.append("\n	" + middle)
 
 	body = after
+
+output.append(
+"\n	return std.string.join(builder, \"\"); \n" +
+"}")
+
+out_file = open("index.poop.d", 'w')
+
+for fragment in output:
+	out_file.write(fragment)
+
+out_file.close()
+
+
+
 
