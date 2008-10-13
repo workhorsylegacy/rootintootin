02aad3a263523b556e47ce3787fca374895b0da4
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-10-13 09:04:46 -0700

    Added the server response date and status fields.

--- rail_cannon_server.d
+++ rail_cannon_server.d
@@ -3,6 +3,7 @@
 import std.stdio;
 import std.socket;
 import std.regexp;
+import std.date;
 
 import rail_cannon;
 
@@ -41,25 +42,77 @@ public class Server {
 	private static Request _request = null;
 	private static Socket _client_socket = null;
 
+	public static string[ushort] status_code;
+
+	public static this() {
+		status_code[404] = "Not Found";
+		status_code[100] = "Continue";
+		status_code[101] = "Switching Protocols";
+		status_code[200] = "OK";
+		status_code[201] = "Created";
+		status_code[202] = "Accepted";
+		status_code[203] = "Non-Authoritative Information";
+		status_code[204] = "No Content";
+		status_code[205] = "Reset Content";
+		status_code[206] = "Partial Content";
+		status_code[300] = "Multiple Choices";
+		status_code[301] = "Moved Permanently";
+		status_code[302] = "Found";
+		status_code[303] = "See Other";
+		status_code[304] = "Not Modified";
+		status_code[305] = "Use Proxy";
+		status_code[307] = "Temporary Redirect";
+		status_code[400] = "Bad Request";
+		status_code[401] = "Unauthorized";
+		status_code[402] = "Payment Required";
+		status_code[403] = "Forbidden";
+		status_code[404] = "Not Found";
+		status_code[405] = "Method Not Allowed";
+		status_code[406] = "Not Acceptable";
+		status_code[407] = "Proxy Authentication Required";
+		status_code[408] = "Request Time-out";
+		status_code[409] = "Conflict";
+		status_code[410] = "Gone";
+		status_code[411] = "Length Required";
+		status_code[412] = "Precondition Failed";
+		status_code[413] = "Request Entity Too Large";
+		status_code[414] = "Request-URI Too Large";
+		status_code[415] = "Unsupported Media Type";
+		status_code[416] = "Requested range not satisfiable";
+		status_code[417] = "Expectation Failed";
+		status_code[500] = "Internal Server Error";
+		status_code[501] = "Not Implemented";
+		status_code[502] = "Bad Gateway";
+		status_code[503] = "Service Unavailable";
+		status_code[504] = "Gateway Time-out";
+		status_code[505] = "HTTP Version not supported";
+	}
+
+	public static string get_verbose_status_code(ushort code) {
+		return std.string.toString(code) ~ " " ~ status_code[code];
+	}
+
 	public static void render_text(string text) {
 		// If we have already rendered, show an error
 		if(_has_rendered) {
 			throw new Exception("This action has already rendered.");
 		}
 
+		string status = get_verbose_status_code(200);
+
 		// Add the HTTP headers
 		string[] reply = [
-		"HTTP/1.1 200 OK\r\n", 
-		"Date: Mon, 13 Oct 2008 04:05:25 GMT\r\n", 
+		"HTTP/1.1 ", status, "\r\n", 
+		"Date: ", std.date.toUTCString(std.date.getUTCtime()), "\r\n", 
 		"Server: Rail Cannon Server0.0\r\n", 
 		"Set-Cookie: _appname_session=BAh7BzoMY3NyZl9pZCIlOWQ0Njc5ODIyNWM5MWZhNGU4OTY4NjczNmEwMTlh%0ANjAiCmZsYXNoSUM6J0FjdGlvbkNvbnRyb2xsZXI6OkZsYXNoOjpGbGFzaEhh%0Ac2h7AAY6CkB1c2VkewA%3D--eb5d809fcaceee78af495aa7544242ba4415a072; path=/\r\n",
-		"Status: 200 OK\r\n",
-		"X-Runtime: 0.15560\r\n",
-		"ETag: \"53e91025a55dfb0b652da97df0e96e4d\"\r\n",
-		"Cache-Control: private, max-age=0, must-revalidate\r\n",
+		"Status: ", status, "\r\n",
+		//"X-Runtime: 0.15560\r\n",
+		//"ETag: \"53e91025a55dfb0b652da97df0e96e4d\"\r\n",
+		//"Cache-Control: private, max-age=0, must-revalidate\r\n",
 		"Content-Type: text/html; charset=utf-8\r\n",
 		"Content-Length: ", std.string.toString(text.length), "\r\n",
-		"Vary: User-Agent\r\n",
+		//"Vary: User-Agent\r\n",
 		"\r\n",
 		text];
 
@@ -67,27 +120,6 @@ public class Server {
 	}
 
 	public static void start(void function(Request request, void function(string) render_text) run_action) {
-		/*
-		 respStatus.Add(200, "200 Ok");
-		 respStatus.Add(201, "201 Created");
-		 respStatus.Add(202, "202 Accepted");
-		 respStatus.Add(204, "204 No Content");
-
-		 respStatus.Add(301, "301 Moved Permanently");
-		 respStatus.Add(302, "302 Redirection");
-		 respStatus.Add(304, "304 Not Modified");
-		 
-		 respStatus.Add(400, "400 Bad Request");
-		 respStatus.Add(401, "401 Unauthorized");
-		 respStatus.Add(403, "403 Forbidden");
-		 respStatus.Add(404, "404 Not Found");
-
-		 respStatus.Add(500, "500 Internal Server Error");
-		 respStatus.Add(501, "501 Not Implemented");
-		 respStatus.Add(502, "502 Bad Gateway");
-		 respStatus.Add(503, "503 Service Unavailable");
-
-		*/
 		const int MAX_CONNECTIONS = 100;
 		ushort port = 2345;
 		TcpSocket listener = new TcpSocket();
