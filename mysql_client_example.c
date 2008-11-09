5c567e7db5e4497bbf177f130bde395fb9af3290
Matthew Brennan Jones <mattjones@workhorsy.org>
2008-11-09 14:31:38 -0800

    Cleaned up the mysql wrapper library. Now it frees most resource. Functions have better names. Functions use the number of rows and columns in the query results.

--- mysql_client_example.c
+++ mysql_client_example.c
@@ -11,7 +11,7 @@ MYSQL mysql;
 MYSQL_RES *res;
 MYSQL_ROW row;
 
-void thing_connect(char* server, char* user_name, char* password, char* database) {
+void db_connect(char* server, char* user_name, char* password, char* database) {
 	mysql_init(&mysql);
 	mysql_real_connect(&mysql,
 					server, 
@@ -20,31 +20,31 @@ void thing_connect(char* server, char* user_name, char* password, char* database
 					0, NULL, 0);
 }
 
-// Runs the query and restuns it as a 3D array of characters
-char*** thing_query(char* query_sql) {
+// Runs the query and returns it as a 3D array of characters
+char*** db_query(char* query, int* row_len, int* col_len) {
 	char*** retval;
 
 	// Run the query and get the result
-	mysql_real_query(&mysql, query_sql, (unsigned int)strlen(query_sql));
+	mysql_real_query(&mysql, query, (unsigned int)strlen(query));
 	res = mysql_store_result(&mysql);
 
-	// Allocated enough memory to hold the pointers for each row into the return value
+	// Allocate enough memory to hold the pointers for each row in the return value
+	int col_count = 0;
 	int row_count = mysql_num_rows(res);
 	retval = (char ***) calloc(row_count, sizeof (char **));
 
 	// Iterate through each row
 	unsigned int row_cur = 0;
 	while(row = mysql_fetch_row(res)) {
-		// Allocated enough memory to hold the pointers for each column into the return value
-		unsigned int col_count = mysql_num_fields(res);
+		// Allocate enough memory to hold the pointers for each column in the return value
+		col_count = mysql_num_fields(res);
 		retval[row_cur] = (char **) calloc(col_count, sizeof (char *));
 
-		// Copy each row into the return calue
+		// Copy each field into the column of the return value
 		int i = 0;
 		for(i=0; i<col_count; i++) {
 			retval[row_cur][i] = (char *) calloc(strlen(row[i])+1, sizeof(char));
 			strcpy(retval[row_cur][i], row[i]);
-			printf("row: %s\n", row[i]);
 		}
 
 		row_cur++;
@@ -53,22 +53,42 @@ char*** thing_query(char* query_sql) {
 	// Free the resources for the result
 	mysql_free_result(res);
 
-	// Return the result
+	// Return the result, number of rows, and columns
+	*row_len = row_count;
+	*col_len = col_count;
 	return retval;
 }
 
+// FIXME: This does not seem to free all of the resources
+void free_db_query(char*** result, int row_len, int col_len) {
+	int i, j;
+	for(i=0; i < row_len; i++) {
+		for(j=0; j < col_len; j++) {
+			free(result[i][j]);
+		}
+		result[i];
+	}
+	free(result);
+}
+
 int main() {
-	thing_connect("localhost", "root", "letmein", "me_love_movies_development");
+	db_connect("localhost", "root", "letmein", "me_love_movies_development");
 
-	char* query_sql = "select id, name from titles order by id limit 30;";
+	char* query = "select id, name from titles order by id limit 30;";
 
-	int n = 10000;
+	int n = 1;
 	while(n > 0) {
-		char*** result = thing_query(query_sql);
+		int row_len, col_len;
+		char*** result = db_query(query, &row_len, &col_len);
+
+		int i, j;
+		for(i=0; i<row_len; i++) {
+			for(j=0; j<col_len; j++) {
+				printf("%s\n", result[i][j]);
+			}
+		}
 		
-		// Free all the memory for the strings
-		// FIXME: This needs to free the all the child strings too
-		free(result);
+		free_db_query(result, row_len, col_len);
 
 		n--;
 	}
