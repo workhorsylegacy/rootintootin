05ff4e2c5f6573bbc3f0e79cb9c6d8068e09dda4
Matthew Brennan Jones <mattjones@workhorsy.org>
2009-03-14 09:19:48 -0700

    Tweaked the gen and build script's requirements helper to be a little more clear when development libraries are not installed.

--- src/helper.py
+++ src/helper.py
@@ -7,43 +7,40 @@ import platform
 class Helper(object):
 	def require_dependencies(other_globals):
 		if platform.system() == 'Linux':
+			# Make sure we have all the requirements installed
+			os_name = "unknown"
+
+			# Make sure we can get the OS name
+			if commands.getoutput('which') != '':
+				print "The command 'which' is not in your path. Exiting ..."
+				exit()
+			elif commands.getoutput('which lsb_release') != '':
+				os_name = commands.getoutput('lsb_release -is')
+			elif commands.getoutput('test -f /etc/fedora-release; echo $?') == '0':
+				os_name = "Fedora"
+			elif commands.getoutput('test -f /etc/SuSE-release; echo $?') == '0':
+				os_name = "SuSE"
+			elif commands.getoutput('test -f /etc/mandriva-release; echo $?') == '0':
+				os_name = "Mandriva"
+			elif commands.getoutput('test -f /etc/distro-release; echo $?') == '0':
+				os_name = commands.getoutput('cat /etc/distro-release')
+			elif commands.getoutput('test -f /etc/*-release; echo $?') == '0':
+				os_name = str.split(str.split(commands.getoutput('cat /etc/*-release'), "DISTRIB_ID=")[1], "\n")[0]
+
+
 			try:
 				other_globals['pexpect'] = __import__("pexpect")
 				other_globals['MySQLdb'] = __import__("MySQLdb")
 
-				# Make sure gdc, and gcc exists
-				for command in ['gdc', 'gcc']:
+				# Make sure gdc exists
+				for command in ['gcc', 'gdc']:
 					if commands.getoutput("which " + command) == '':
 						raise Exception('')
 
-				# Make sure the mysql and tango libs are installed
-				for lib in ["'/usr/include/mysql/mysql.h'", "'/usr/lib/tango-gdc/libgtango.a'"]:
-					if commands.getoutput('test -f ' + lib + '; echo $?') == '1':
-						raise Exception('')
 			except:
-				# Make sure we have all the requirements installed
-				os_name = "unknown"
-
-				# Make sure we can get the OS name
-				if commands.getoutput('which') != '':
-					print "The command 'which' is not in your path. Exiting ..."
-					exit()
-				elif commands.getoutput('which lsb_release') != '':
-					os_name = commands.getoutput('lsb_release -is')
-				elif commands.getoutput('test -f /etc/fedora-release; echo $?') == '0':
-					os_name = "Fedora"
-				elif commands.getoutput('test -f /etc/SuSE-release; echo $?') == '0':
-					os_name = "SuSE"
-				elif commands.getoutput('test -f /etc/mandriva-release; echo $?') == '0':
-					os_name = "Mandriva"
-				elif commands.getoutput('test -f /etc/distro-release; echo $?') == '0':
-					os_name = commands.getoutput('cat /etc/distro-release')
-				elif commands.getoutput('test -f /etc/*-release; echo $?') == '0':
-					os_name = str.split(str.split(commands.getoutput('cat /etc/*-release'), "DISTRIB_ID=")[1], "\n")[0]
-
 				if ['Ubuntu', 'Debian'].count(os_name):
 					print "Please install requirements:\n" + \
-							"    sudo apt-get install mysql-client mysql-server libmysqlclient15-dev python-pexpect python-mysqldb gcc"
+							"    sudo apt-get install mysql-client mysql-server libmysqlclient15-dev python-pexpect python-mysqldb gcc gdc"
 					exit()
 				elif ['Fedora'].count(os_name):
 					print "Please install requirements:\n" + \
@@ -57,6 +54,16 @@ class Helper(object):
 					print "Please install requirements for your unknown Linux distro:\n" + \
 							"    mysql-client, mysql-server, libmysqlclient15-dev, python-pexpect, python-mysqldb, gcc, tango-gdc, and gdc"
 					exit()
+
+			# Make sure the mysql libs are installed
+			if commands.getoutput('test -f ' + "'/usr/include/mysql/mysql.h'" + '; echo $?') == '1':
+				print "Please install the MySQL development libraries."
+				exit()
+
+			# Make sure the tango libs are installed
+			if commands.getoutput('test -f ' + "'/usr/lib/tango-gdc/libgtango.a'" + '; echo $?') == '1':
+				print "Please install the GDC Tango development libraries."
+				exit()
 
 		elif platform.system() == 'Windows':
 			print 'Does not yet work on Windows. Exiting ...'
