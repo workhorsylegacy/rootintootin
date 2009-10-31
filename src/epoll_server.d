1d75fda83535e367997cb7bb86b4c0ba6aed3ddf
Matthew Brennan Jones <mattjones@workhorsy.org>
2009-10-31 13:02:01 -0700

    Cleaned up the code for the epoll sever toy.

--- src/epoll_server.d
+++ src/epoll_server.d
@@ -27,42 +27,53 @@ public class SocketThread : Thread {
 
 
 public class TcpServer {
-	private	uint _max_threads;
-	private	uint _port;
-	private	uint _max_waiting_clients;
-	private	ServerSocket _server;
-	private	EpollSelector _selector;
+	private	ushort _port;
+	private	ushort _max_waiting_clients;
+	private	ServerSocket _server = null;
+	private	EpollSelector _selector = null;
 
-	public this(uint port, uint max_threads, uint max_waiting_clients) {
-		this._max_threads = max_threads;
+	public this(ushort port, ushort max_waiting_clients) {
 		this._port = port;
 		this._max_waiting_clients = max_waiting_clients;
-		this._server = new ServerSocket(new InternetAddress(_port), _max_waiting_clients, true);
 	}
 
 	public void start() {
+		// Create a server socket that is non-blocking, can re-use dangling addresses, and can hold many connections.
 		Socket client = null;
-		this._selector = new EpollSelector();
-		this._selector.open(); //open(10, 3);
+		this._server = new ServerSocket(new InternetAddress(this._port), this._max_waiting_clients, true);
 		this._server.socket.blocking(false);
 
+		// Create an epoll selector
+		this._selector = new EpollSelector();
+		this._selector.open(); //open(10, 3);
 		Stdout.format("Running on http://localhost:{} ...\n", this._port).flush;
+
 		while(true) {
-			this._selector.register(this._server, Event.Read | Event.Hangup | Event.Error | Event.InvalidHandle);
+			// Wait forever for any read, hangup, error, or invalid handle events
+			this._selector.register(this._server, Event.Read | Event.Hangup | Event.Error | Event.InvalidHandle);;
 			if(this._selector.select(-1) == 0) {
 				continue;
 			}
 
+			// Respond to any accepts or errors
 			foreach(SelectionKey item; this._selector.selectedSet()) {
 				if(item.conduit is this._server) {
 					client = (cast(ServerSocket) item.conduit).accept();
-					SocketThread thread = new SocketThread(client, this._selector);
-					thread.start();
+
+					try {
+						SocketThread thread = new SocketThread(client, this._selector);
+						thread.start();
+					} catch(tango.core.Exception.ThreadException err) {
+						client.write("500: Too many connections");
+						client.shutdown();
+						client.detach();
+						this._selector.unregister(item.conduit);
+					}
 				} else if(item.isError() || item.isHangup() || item.isInvalidHandle()) {
 					Stdout("FIXME: error, hangup, or invalid handle").flush;
 					this._selector.unregister(item.conduit);
 				} else {
-					Stdout("FIXME: unexpected result from selector.selectedSet()");
+					Stdout("FIXME: unexpected result from selector.selectedSet()").flush;
 				}
 			}
 		}
@@ -72,7 +83,10 @@ public class TcpServer {
 
 
 void main() {
-	TcpServer server = new TcpServer(3000, 10, 1000);
+	ushort port = 3000;
+	ushort max_waiting_clients = 1000;
+
+	TcpServer server = new TcpServer(port, max_waiting_clients);
 	server.start();
 }
 
